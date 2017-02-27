9e612f5600e2ba93e43511e8735f6d2ac7334499
andela-akolawole <aladekolawole15@yahoo.com>
2017-02-27 13:39:56 +0100

    add cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 require('dotenv').config();
 
 var index = require('./routes/index');
@@ -25,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());
 app.use('/', index);
 app.use('/api', api);
 
